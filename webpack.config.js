2a7c57c9cc5511bc2091567ecf06594679554815
ILya Cyclone <ilya.cyclone@gmail.com>
2021-02-25 14:27:23 +0300

    configure webpack and vscode debugging
    Webpack now builds console calculator.

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,28 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.tsx",
-    resolve: {
-      extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
-    },
-    output: {
-      path: path.join(__dirname, "dist"),
-      filename: "index.js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|ts)x?$/,
-                loader: "babel-loader",
-                exclude: /node_modules/
-            }
-        ]
-    },
-    devServer: {
-      historyApiFallback: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./src/index.html",
-      }),
+  mode: "production",
+  target: "node",
+  devtool: "source-map",
+  entry: "./src/calc/index.ts",
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "calc.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      }
     ]
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 };
